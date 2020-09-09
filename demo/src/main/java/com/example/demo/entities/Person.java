ed7b2386ad134ca973b3c594e3155983f1c3fd87
Thaires <thaires.agoncalves@gmail.com>
2020-09-09 15:23:09 -0400

    remove import desnecessario

--- demo/src/main/java/com/example/demo/entities/Person.java
+++ demo/src/main/java/com/example/demo/entities/Person.java
@@ -14,7 +14,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 
